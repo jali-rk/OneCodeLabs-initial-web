0712ac4a13e256b5e3f1109a988936aae1bdf29e
jali-rk <jaliya.r.kumarasiri@gmail.com>
2026-02-14 19:46:17 +0530

    Replace assets in

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './', // Use relative paths for GitHub Pages deployment
+      base: process.env.NODE_ENV === 'production' ? '/OneCodeLabs-initial-web/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
